27f71065e117338d94def85a6cfec05cdd81cdb3
nhk9710 <hknoh@wkit.co.kr>
2022-02-23 14:11:31 +0900

    [add]many things

--- js/main.js
+++ js/main.js
@@ -8,34 +8,58 @@ $(document).ready(() => {
     $html.animate({scrollTop:0},10);
 
     window.addEventListener("wheel", function (e){
+
         e.preventDefault();
     }, {passive: false});
 
     $(window).on("wheel",function (e){
         if ($html.is(":animated")) return;
 
-        if(e.originalEvent.deltaY > 0){
+        if(e.originalEvent.deltaY > 0){ //아래쪽으로 굴린거
             if(page === lastPage) return;
-
             page++;
-        }else if(e.originalEvent.deltaY < 0){
-            if(page === 1) return;
 
+            if( $('.content-wise').hasClass('content-ani')){
+
+                $('.content-wise').removeClass('content-ani')
+            }else{
+                $('.content-wise').addClass('content-ani')
+            }
+        }else if(e.originalEvent.deltaY < 0){ //위쪽으로 굴린거
+            if(page === 1) return;
             page--;
+
+            if( $('.content-wise').hasClass('content-ani')){
+                console.log(1)
+                $('.content-wise').removeClass('content-ani')
+            }else{
+                console.log(2)
+                $('.content-wise').addClass('content-ani')
+            }
         }
         let posTop = (page-1) * $(window).height();
 
         $html.animate({scrollTop : posTop});
     })
-
-    const burger = $('.menu-trigger');
-
-    burger.each(function(index){
+    /* trigger */
+    const content = $('.content-trigger');
+    content.each(function(index){
         let $this = $(this);
 
         $this.on('click', function(e){
             e.preventDefault();
-            $(this).toggleClass('active-' + (index+1));
+            $(this).toggleClass('active');
         })
     });
+    $('.content-trigger').click(function(){
+        if( $('.content-hidden').hasClass('content-text')){
+            $('.content-hidden').removeClass('content-text')
+        }else{
+            $('.content-hidden').addClass('content-text')
+        }
+    });
+
+
 })
+
+
